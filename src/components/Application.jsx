159e078ee4fce25a8732c55d22657eb18a134a53
Saam Koukpari <saam.koukpari@gmail.com>
2022-02-10 13:53:14 -0800

    add component structure

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import './Application.scss';
+import DayList from "./DayList";
 
 
 export default function Application() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
         <div className="sidebar__header">
-        <img
-            className="sidebar--logo"
-            src="images/logo.png"
-            alt="To Do List"
-          />Quick-To-Do
+          <img
+              className="sidebar--logo"
+              src="images/logo.png"
+              alt="To Do List"
+            />Quick-To-Do
         </div>
         <hr className="sidebar__separator sidebar--centered"/>
+        <nav className="sidebar__menu">
+          <DayList/>
+        </nav>
       
       </section>
     </main>
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+//import classNames from "classnames";
 
 
-export default function DayListItem() {
+export default function DayListItem(props) {
+
+
   return (
     <div>
 

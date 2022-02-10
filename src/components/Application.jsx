992d43291b6404f54318ec3fbc88a76f21422898
Saam Koukpari <saam.koukpari@gmail.com>
2022-02-10 13:31:25 -0800

    add sidebar & sass styling

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -4,9 +4,17 @@ import './Application.scss';
 export default function Application() {
   return (
     <main className="layout">
-      <div className="sidebar">
+      <section className="sidebar">
+        <div className="sidebar__header">
+        <img
+            className="sidebar--logo"
+            src="images/logo.png"
+            alt="To Do List"
+          />Quick-To-Do
+        </div>
+        <hr className="sidebar__separator sidebar--centered"/>
       
-      </div>
+      </section>
     </main>
   );
 };

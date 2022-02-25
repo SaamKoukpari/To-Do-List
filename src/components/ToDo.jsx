dd12147ea7ff45cc84e91e3d9de8894b35f3035a
Saam Koukpari <saam.koukpari@gmail.com>
2022-02-25 10:34:23 -0800

    edit styling

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+import "./ToDo.scss"
+
+
+export default function ToDo(props) {
+  
+  
+  return (
+    <li className="todo">
+      <div className="c-cb">
+        <input id="todo-0" type="checkbox" defaultChecked={true} />
+        <label className="todo-label" htmlFor="todo-0">
+          Eat
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">Eat</span>
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">Eat</span>
+        </button>
+      </div>
+    </li>
+  )
+}
